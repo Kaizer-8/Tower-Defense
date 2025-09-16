334bfa775d3194d2691394cd1f2acc9f0bb4f211
Iso ( purple ) <tobiasbakker2007@gmail.com>
2025-09-16 13:42:34 +0200

    Towers shoot now
    -NOTE: towers shoot while being placed. will fix later lolz

--- Assets/Monoscript/TowerBehavior.cs
+++ Assets/Monoscript/TowerBehavior.cs
@@ -1,16 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerBehavior : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject Projectile;
+    public Transform Tower;
+    public float fireRate = 1f; 
+    public float range = 5f;
 
-    // Update is called once per frame
+    private float fireCooldown;
+
     void Update()
     {
-        
+        fireCooldown -= Time.deltaTime;
+        Transform target = FindClosestEnemy();
+
+        if (target && fireCooldown <= 0f)
+        {
+            TowerProjectile.Spawn(Projectile, Tower.position, Quaternion.identity, target);
+            fireCooldown = 1f / fireRate;
+        }
+    }
+
+    Transform FindClosestEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        Transform closest = null;
+        float minDist = Mathf.Infinity;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float dist = Vector3.Distance(transform.position, enemy.transform.position);
+            if (dist < minDist && dist <= range)
+            {
+                minDist = dist;
+                closest = enemy.transform;
+            }
+        }
+
+        return closest;
     }
 }
